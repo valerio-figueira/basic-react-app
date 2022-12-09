68d95d81f4e395f8585885e2cb3333f5ab0457d2
valerio-figueira <j.valerio.figueira@gmail.com>
2022-12-09 10:24:00 -0300

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Fragmento from './components/basicos/Fragmento';
 import MaxAndMin from './components/basicos/MaxAndMin';
 import Card from './components/layout/Card';
 
-const helloWorld = <h1>Hello, world!</h1>
+const helloWorld = <h1>Fundamentos React</h1>
 
 function getRandom(){
   return Math.floor(Math.random() * 10)
@@ -20,23 +20,39 @@ const [n1, n2] = [getRandom(), getRandom()];
 function App() {
   return (
     <div className='container'>
-        {helloWorld}
-        <Componente />
-        <ComParametro 
-        titulo="Segundo Componente" 
-        subtitulo="Este é um componente com parâmetros. Chamados também de props (propriedades)."
-        />
-        <Aluno
-        nome="José Valério"
-        nota={7.5} />
-        <Fragmento />
-        <MaxAndMin
-        max={n1 > n2 ? n1 : n2}
-        min={n1 < n2 ? n1 : n2} />
 
-        <Card
-        titulo="Exemplo de Card"
-        />
+        {helloWorld}
+        
+        <div className="cards">
+            <Card titulo="1º Componente">
+              <Componente />
+            </Card>
+
+            <Card titulo="Parâmetro">
+              <ComParametro 
+              titulo="Segundo Componente" 
+              subtitulo="Este é um componente com parâmetros. Chamados também de props (propriedades)."
+              />
+            </Card>
+
+            <Card titulo="Aluno">
+              <Aluno
+                nome="José Valério"
+                nota={7.5}
+              />
+            </Card>
+            
+            <Card titulo="Fragmento">
+              <Fragmento />
+            </Card>
+
+            <Card titulo="Desafio Valores Aleatórios">
+              <MaxAndMin
+              max={n1 > n2 ? n1 : n2}
+              min={n1 < n2 ? n1 : n2} />
+            </Card>
+        </div>
+
     </div>
   )
 }
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -5,9 +5,9 @@ function Card(props){
     return (
         <div className='card'>
             <div className='title'>{props.titulo}</div>
-            <div className='content'>Lorem ipsum dolor.</div>
+            <div className='content'>{props.children}</div>
         </div>
     )
 }
 
-export default Card
+export default Card;
